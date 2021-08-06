876842ac74095b9e4b54cfa6ace3ff4c3191f61c
usk-155 <y.nakajima.810@gmail.com>
2021-08-06 13:51:16 +0900

    headerの日付入力編集

--- tenki_app/main.js
+++ tenki_app/main.js
@@ -1,14 +1,43 @@
-// XMLHttpRequestオブジェクト を fetchに書き換え
+// headerの日付入力
 
 // 取得したAPI Keyをここに記載
-let apiKey = "cc05750ba50400f27ebabbcd6f4c4976";       // ------------ ①
+let apiKey = "a533675e07f5f09476d1b1b748f2b159";       // ------------ ①
 let resortName = ["蔵王温泉スキー場", "苗場スキー場", "赤倉観光リゾート", ];//緯度
 let lat = ["38.1704429", "36.7916707", "36.8866025", ];//経度
 let lon = ["140.3992911", "138.7799397", "138.1736814", ];//緯度
 let url = "https://api.openweathermap.org/data/2.5/forecast?lat=" + lat[0] + "&lon=" + lon[0] + "&units=metric&lang=ja&appid=" + apiKey ;
 
+// date format
+let formatDate = (dateTime) => {
+    let formatted_date = dateTime.getFullYear() + "/" + (dateTime.getMonth() + 1).toString().padStart(2, '0') + "/" + dateTime.getDate().toString().padStart(2, '0') + " " + dateTime.getHours().toString().padStart(2, '0') + ":" + dateTime.getMinutes().toString().padStart(2, '0');
+    return formatted_date;
+}
+
+// todayNow(今日)の日付時間 取得
+let today = new Date();
+const todayNow = document.getElementById("todayNow");
+let todayUnix = today.getTime()/1000; // 【 今日のUnixTime 】
+// todayをHTMLに出力
+todayNow.textContent = "TODAY : " + formatDate(today); 
+
+console.log(todayUnix);
+
+// checkInの日付09時 取得　/　checkInUnix と todayUnix の時間差(整数値)を取得
+document.getElementById("checkIn").onchange = function () {
+    let checkInUnix = document.getElementById("checkIn").valueAsNumber/1000; // 【 checkInのUnixTime 】
+    timeDiff = parseInt((checkInUnix - todayUnix)/3600); //時間座(整数値)
+}; 
+
+// (checkIn-todayNow)/3 => 整数　=2 → list2からスタート
+// 0,1 は空白にする
+
+
+
+
+
 function buttonClick(value) {
     let url = "https://api.openweathermap.org/data/2.5/forecast?lat=" + lat[value] + "&lon=" + lon[value] + "&units=metric&appid=" + apiKey;
+    // https://api.openweathermap.org/data/2.5/forecast?lat=38.1704429&lon=140.3992911&units=metric&appid=a533675e07f5f09476d1b1b748f2b159
     fetch(url, {
         method: "GET",
     })
@@ -106,3 +135,5 @@ function buttonClick(value) {
         div.innerHTML = html;
     });
 };
+
+
